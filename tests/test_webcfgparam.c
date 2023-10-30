3b1941de6eec6ba024efaaf252b57e9ec9850070
Rangaiah <narapuram_lakshmirangaiah@comcast.com>
2023-10-30 11:02:04 +0530

    Add unit tests for webcfg_param.c

--- tests/test_webcfgparam.c
+++ tests/test_webcfgparam.c
@@ -18,14 +18,17 @@
 #include <errno.h>
 #include <stdio.h>
 #include <CUnit/Basic.h>
+#include <msgpack.h>
 #include "../src/webcfg_param.h"
+#define WEB_CFG_FILE "../../tests/wanmanager.bin"
 
-#define WEB_CFG_FILE "../../tests/webcfg-now100.bin"
-int readFromFile(char **data, int *len)
+int process_webcfgparam( webcfgparam_t *pm, msgpack_object *obj );
+
+int readFromFile(char *filename,char **data, int *len)
 {
 	FILE *fp;
 	int ch_count = 0;
-	fp = fopen(WEB_CFG_FILE, "r+");
+	fp = fopen(filename, "r+");
 	if (fp == NULL)
 	{
 		printf("Failed to open file %s. provide the file\n", WEB_CFG_FILE);
@@ -52,7 +55,7 @@ void test_basic()
 	char *binfileData = NULL;
 	int status = -1;
 	printf("readFromFile\n");
-	status = readFromFile(&binfileData , &len);
+	status = readFromFile(WEB_CFG_FILE,&binfileData , &len);
 
 	printf("read status %d\n", status);
 	if(status)
@@ -63,12 +66,12 @@ void test_basic()
 
 		pm = webcfgparam_convert( basicV, len+1 );
 		err = errno;
-		printf( "errno: %s\n", webcfgparam_strerror(err) );
+		printf( "errno: %s\n", webcfgparam_strerror(err) );		
+
 		CU_ASSERT_FATAL( NULL != pm );
 		CU_ASSERT_FATAL( NULL != pm->entries );
-		CU_ASSERT_FATAL( 100 == pm->entries_count );
+		CU_ASSERT_FATAL( 2 == pm->entries_count );
 		CU_ASSERT_STRING_EQUAL( "Device.DeviceInfo.X_RDKCENTRAL-COM_CloudUIEnable", pm->entries[0].name );
-		CU_ASSERT_STRING_EQUAL( "false", pm->entries[0].value );
 		CU_ASSERT_FATAL( 3 == pm->entries[0].type );
 		for(i = 0; i < (int)pm->entries_count ; i++)
 		{
@@ -81,10 +84,71 @@ void test_basic()
 	}
 }
 
+void test_webcfgparam_strerror()
+{
+	const char *txt;
+	txt = webcfgparam_strerror(0);
+	CU_ASSERT_STRING_EQUAL(txt,"No errors.");
+	txt = webcfgparam_strerror(1);
+	CU_ASSERT_STRING_EQUAL(txt,"Out of memory.");	
+	txt = webcfgparam_strerror(10);
+	CU_ASSERT_STRING_EQUAL(txt,"Unknown error.");	
+}
+
+void test_process_webcfgparam()
+{
+	msgpack_object *inner;
+	webcfgparam_t *pm;
+	int ret=0,err=0;
+	inner = malloc( sizeof(msgpack_object));
+	pm = malloc( sizeof(webcfgparam_t));
+	inner->via.array.size=0;
+	ret = process_webcfgparam(pm,inner);
+        CU_ASSERT_EQUAL(ret, 0);
+
+	inner->via.array.size=1;
+	msgpack_object_array *array = &inner->via.array;
+	array->ptr = malloc( sizeof(msgpack_object));
+	array->ptr->type = 0;
+	ret = process_webcfgparam(pm,inner);
+        CU_ASSERT_EQUAL(ret, -1);
+	err = errno;
+	const char *txt;
+	txt = webcfgparam_strerror(err);
+	printf( "errno: %s\n", txt );
+	CU_ASSERT_STRING_EQUAL(txt,"Invalid 'parameters' array.");
+}
+
+void test_process_params_invalid_datatype()
+{
+	int err, len=0;
+	char *binfileData = NULL;
+	int status = -1;
+	printf("readFromFile\n");
+	status = readFromFile("../../tests/test1.bin",&binfileData , &len);
+
+	printf("read status %d\n", status);
+	if(status)
+	{
+		void* basicV;
+
+		basicV = ( void*)binfileData;
+
+		webcfgparam_convert( basicV, len+1 );
+		err = errno;
+		const char *txt;
+		txt = webcfgparam_strerror(err);	
+		CU_ASSERT_STRING_EQUAL(txt,"Invalid 'blob' object.");					
+	}
+}
+
 void add_suites( CU_pSuite *suite )
 {
     *suite = CU_add_suite( "tests", NULL, NULL );
     CU_add_test( *suite, "Full", test_basic);
+	CU_add_test( *suite, "test webcfgparam_strerror", test_webcfgparam_strerror);
+	CU_add_test( *suite, "test process_webcfgparam", test_process_webcfgparam);	
+    CU_add_test( *suite, "test process_params", test_process_params_invalid_datatype);	
 }
 
 /*----------------------------------------------------------------------------*/
@@ -116,3 +180,4 @@ int main( int argc, char *argv[] )
 
     return rv;
 }
+
